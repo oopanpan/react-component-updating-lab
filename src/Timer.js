d31f685353c06792c59b7baeda3b4aab33ba2adb
oopanpan <oopanpan@gmail.com>
2021-03-17 22:27:53 -0700

    commit tree d5c6eeca640b29fbfab9d7d25aa48c6591cc32b9

--- src/Timer.js
+++ src/Timer.js
@@ -11,6 +11,16 @@ class Timer extends Component {
   }
 
   //Your code here
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.time !== nextState.time){
+      return true
+    }
+    return false
+  }
+
+  componentDidUpdate() {
+    this.timer.current.style.color =  "#" + Math.floor(Math.random() * 16777215).toString(16)
+  }
 
   componentDidMount() {
     this.interval = setInterval(
